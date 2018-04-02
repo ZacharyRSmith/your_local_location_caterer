6ac6a1044e5a0cd99bfea6ff2e890d0d8a8bbd0d
Zachary R. Smith <ZacharySmith4989@gmail.com>
2018-04-02 00:48:31 -0400

    fix: Pagination doesn't reload entire page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'locations', to: 'locations#index', defaults: { format: 'json' }
   post 'locations', to: 'locations#create', defaults: { format: 'json' }
 
   root 'application#hello'
